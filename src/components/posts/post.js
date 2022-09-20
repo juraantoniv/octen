6770ec61fd66dcb946b3c136f9c4711035aa413e
juraantoniv <juraantoniv@gmail.com>
2022-09-20 23:12:34 +0200

    ok

--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -1,12 +1,11 @@
 
-export default function Post ({item,key}){
+export default function Post ({item}){
     const {title,body} = item;
     let box_s ='box_s'
 
     return (
 
         <div className={box_s}>
-            <div>id:{key}</div>
             <div>title:{title}</div>
             <div>body:{body}</div>
         </div>
